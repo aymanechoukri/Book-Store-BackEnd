e424402ea711c30b799d2683910994b0f81a1c86
aymanechoukri <aymanedivl@gmail.com>
2026-03-25 12:12:37 +0100

    connect mongoose to mongodb

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-import app from './router/app.js';
+import app from "./router/app.js";
+import connectDB from "./config/db.js";
 
-const PORT = 3000;
+connectDB();
+
+const PORT = 5000;
 
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`)
-})
+  console.log(`http://localhost:${PORT}`);
+});
